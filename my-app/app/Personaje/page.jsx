b8c229505973ce9f080f5edf242c24b69a6d2372
Lucas Sorzio <sorziolucas4@gmail.com>
2026-01-07 20:38:36 -0300

    agregado de paginacion

--- my-app/app/Personaje/page.jsx
+++ my-app/app/Personaje/page.jsx
@@ -1,35 +1,40 @@
-import React from 'react'
+import Link from "next/link"
+import Personajes from "./Personajes"
 
-import Personajes from './Personajes';
-async function obtener() {
+async function obtener(page) {
   const res = await fetch(
-    "https://dragonball-api.com/api/characters"
-   
-  );
-  return res.json();
+    `https://dragonball-api.com/api/characters?page=${page}`,
+    { cache: "no-store" }
+  )
+
+  return res.json()
 }
 
-
-
-
-
-
-async function page() {
-    const datos = await obtener();
-    console.log(datos);
+export default async function Page({ searchParams }) {
+  const { page = 1 } = await searchParams
+  const datos = await obtener(page)
 
   return (
-      <div>
-          
+    <div>
+      <h1>Personajes de Dragon Ball</h1>
 
-        <h1>Personajes de Los dbz</h1>
-        <div className="flex flex-row flex-wrap justify-center p-10 bg-black">
-          {datos.items.map((personaje) => (
-            <Personajes key={personaje.id} personaje={personaje} />
-          ))}
-        </div>
+      <div className="flex flex-wrap justify-center p-10 bg-black">
+        {datos.items.map((personaje) => (
+          <Personajes key={personaje.id} personaje={personaje} />
+        ))}
+      </div>
+
+      <div className="flex gap-4 justify-center">
+              {page > 1 && page < 6 ? (
+                  <div>
+          <Link href={`?page=${Number(page) - 1}`}>Anterior</Link>
+       
+        <Link href={`?page=${Number(page) + 1}`}>Siguiente</Link>
+                 </div>
+              ) :
+                  <Link href={`?page=${Number(page) - 1}`}>Anterior</Link>
+                  }    
+      </div>
     </div>
   )
 }
-
-export default page
