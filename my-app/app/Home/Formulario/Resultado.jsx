2ce0e3d0b2668ae503cd5a26a2c6eca68863c99e
Lucas Sorzio <sorziolucas4@gmail.com>
2026-03-09 22:19:52 -0300

    mejoras en el style del buscador

--- my-app/app/Home/Formulario/Resultado.jsx
+++ my-app/app/Home/Formulario/Resultado.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react"
 import Formu from "./Formu"
 import Personaje from "../../Personaje/Personajes"
-
+import "./Resultado.css"
 const Resultado = () => {
   const [personaje, setPersonaje] = useState("")
   const [filtro, setFiltro] = useState(null)
@@ -29,8 +29,10 @@ console.log(personaje)
     
 console.log(filtro)
   return (
-      <div className="flex- flex-col p-20 items-center justify-center ">
-          
+      <div className="fondo">
+     
+      
+      <h2 className="text-amber-500  font-extrabold  text-4 text-5xl  [-webkit-text-stroke:2px_black]">Bienvenidos al buscador de DBZ</h2>
           
       <Formu obtener={obtener} />
 {!cargando && <p className="text-amber-100">Cargando...</p>}
@@ -45,7 +47,9 @@ console.log(filtro)
               })
             )
               : <p>no existe</p>}
-    </div>
+    
+      </div>
+      
   )
 }
 
